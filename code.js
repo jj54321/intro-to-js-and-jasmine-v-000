db7d18bcbbb7ccd513348ac6f7a6432771674fd7
Jacob <mailme711@gmail.com>
2016-04-06 11:46:14 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
-'use strict';
+function favIceCream(fic){
+  return "I love " + fic;
+}
+
+function shouting(shout){
+  return shout.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
